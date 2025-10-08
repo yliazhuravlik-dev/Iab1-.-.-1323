969b723b7120c02c397216a87fb7198c7e5736ab
yliazhuravlik-dev <yliazhuravlik@gmail.com>
2025-10-08 18:02:43 +0300

    Update Villain.cpp: part 2

--- src/Villain.cpp
+++ src/Villain.cpp
@@ -64,6 +64,88 @@ std::string Villain::getLocation() const {
 }
 
 void Villain::addSkill(const std::string& skill) {
-    // Метод push_back добавляет элемент в конец вектора skills
     skills.push_back(skill);
+}
+
+void Villain::removeSkill(int index) {
+    // Проверяем валидность индекса
+    if (index >= 0 && index < skills.size()) {
+        skills.erase(skills.begin() + index);
+    } else {
+        throw std::out_of_range("Invalid skill index for removal in Villain.");
+    }
+}
+int Villain::getSkillsCount() const {
+    // size() возвращает текущее количество элементов в векторе
+    return skills.size();
+}
+const std::string& Villain::getSkill(int index) const {
+    if (index >= 0 && index < skills.size()) {
+
+        return skills[index];
+    } else {
+        throw std::out_of_range("Invalid skill index in Villain.");
+    }
+}
+
+void Villain::print() const {
+    std::cout << "Villain: " << name << std::endl;
+    std::cout << "Weapon: " << weaponType << std::endl;
+    std::cout << "Crime: " << crime << std::endl;
+    std::cout << "Location: " << location << std::endl;
+    std::cout << "Skills: ";
+
+    if (skills.empty()) {
+        std::cout << "No skills";
+    } else {
+        for (size_t i = 0; i < skills.size(); ++i) {
+            std::cout << skills[i];
+            if (i != skills.size() - 1) {
+                std::cout << "; ";
+            }
+        }
+    }
+    std::cout << "\n-------------------" << std::endl;
+}
+void Villain::saveToFile(std::ofstream& out) const {
+    out << "Villain\n";
+    out << name << "\n";
+    out << weaponType << "\n";
+    out << crime << "\n";
+    out << location << "\n";
+    out << skills.size() << "\n";
+    
+    for (const auto& skill : skills) {
+        out << skill << "\n";
+    }
+}
+
+void Villain::loadFromFile(std::ifstream& in) {
+    std::getline(in, name);
+    std::getline(in, weaponType);
+    std::getline(in, crime);
+    std::getline(in, location);
+    
+    int skillCount;
+    in >> skillCount;
+    in.ignore();
+    
+    skills.clear();
+    
+    std::string skill;
+    for (int i = 0; i < skillCount; ++i) {
+        std::getline(in, skill);
+        skills.push_back(skill);
+    }
+}
+
+Villain& Villain::operator=(const Villain& other) {
+    if (this != &other) {
+        name = other.name;
+        weaponType = other.weaponType;
+        crime = other.crime;
+        location = other.location;
+        skills = other.skills;
+    }
+    return *this;
 }
