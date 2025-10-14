c4b94941d80674eae9db349e05eec6d796fece13
yliazhuravlik-dev <yliazhuravlik@gmail.com>
2025-10-14 20:23:45 +0300

    Update Monster.cpp: part 2

--- src/Monster.cpp
+++ src/Monster.cpp
@@ -16,3 +16,50 @@ Monster::Monster(const Monster& other)
     std::cout << "Monster copy constructor for " << name << std::endl;
 }
 
+Monster::~Monster() {
+    // Вывод информации о вызове деструктора
+    std::cout << "Monster destructor for " << name << std::endl;
+}
+
+void Monster::setName(const std::string& name) {
+    // this->name обращается к полю класса (чтобы отличить от параметра)
+    this->name = name;
+}
+
+std::string Monster::getName() const {
+    // Возвращаем копию строки name
+    return name;
+}
+
+void Monster::setAppearance(const std::string& appearance) {
+    this->appearance = appearance;
+}
+
+std::string Monster::getAppearance() const {
+    return appearance;
+}
+
+void Monster::print() const {
+    std::cout << "Monster: " << name << std::endl;
+    std::cout << "Appearance: " << appearance << std::endl;
+    std::cout << "-------------------" << std::endl;
+}
+
+void Monster::saveToFile(std::ofstream& out) const {
+    out << "Monster\n";
+    out << name << "\n";
+    out << appearance << "\n";
+}
+
+void Monster::loadFromFile(std::ifstream& in) {
+    std::getline(in, name);
+    std::getline(in, appearance);
+}
+
+Monster& Monster::operator=(const Monster& other) {
+    if (this != &other) {
+        name = other.name;
+        appearance = other.appearance;
+    }
+    return *this;
+}
